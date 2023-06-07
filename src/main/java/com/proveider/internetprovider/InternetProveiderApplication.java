44c075cc5a5ceda86fc1832ce1c5079ad414f1e7
Evgeny Norkin <norkinzheny@gmail.com>
2023-06-07 23:35:53 +0300

    Create basic authorization logic

---
+++ src/main/java/com/proveider/internetprovider/InternetProveiderApplication.java
@@ -0,0 +1,13 @@
+package com.proveider.internetprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetProveiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetProveiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proveider/internetprovider/InternetProveiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proveider.internetprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetProveiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
