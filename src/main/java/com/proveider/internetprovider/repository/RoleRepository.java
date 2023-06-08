1bcdc4c795eaf042e47b16b229b795e864f548c6
Evgeny Norkin <norkinzheny@gmail.com>
2023-06-08 15:45:57 +0300

    Update project to thymeleaf, spring Boot 3 and Spring Security 8.

---
+++ src/main/java/com/proveider/internetprovider/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.proveider.internetprovider.repository;
+
+import com.proveider.internetprovider.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
