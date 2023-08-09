71853d646dabce51b081aa4ffa46e9e32e62f00d
Evgeny Norkin <norkinzheny@gmail.com>
2023-08-09 23:31:04 +0300

    fix sql

--- src/main/resources/sql/insert_test_data.sql
+++ src/main/resources/sql/insert_test_data.sql
@@ -9,6 +9,6 @@ INSERT INTO `provider`.`tariffs` (`name`, `description`, `price`, `services_id`)
 INSERT INTO `provider`.`tariffs` (`name`, `description`, `price`, `services_id`) VALUES ('Расширенный', 'Цифровое ТВ (IPTV)', '20', '2');
 INSERT INTO `provider`.`tariffs` (`name`, `description`, `price`, `services_id`) VALUES ('Сотовая связь', 'Городская телефонная связь', '1', '3');
 
-INSERT INTO `provider`.`contact_details` (`id`, `city`, `street`, `home`, `apartment`, `email`, `phone`) VALUES (1,'Kharkiv','Pushkinskaya','23a','12','admin@gmail.com','+375292589898');
+INSERT INTO `provider`.`contact_details` (`id`, `city`, `street`, `home`, `apartment`, `email`, `phone`) VALUES (1,'Minsk','Beleckogo','32','114','admin@gmail.com','+375292589898');
 INSERT INTO `provider`.`accounts` (`id`, `number`, `balance`) VALUES (1,1,0);
 INSERT INTO `provider`.`users` (`login`, `password`, `first_name`, `last_name`, `surname`, `blocked`, `roles_id`, `contact_details_id`, `accounts_id`) VALUES ('admin12345678','admin1',' Ivan ','Ivanovich','Ivanov',0,1,1,1);
